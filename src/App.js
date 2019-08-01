df4d276701168480b9eba0d07f64a1c662759bf0
pawkw <31358773+pawkw@users.noreply.github.com>
2019-07-31 21:39:54 -0400

    Added firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+    componentDidMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyB_l7grbKktK75jVVHOUbkI-SDrMCTkYD8',
+            authDomain: 'auth-15fdf.firebaseapp.com',
+            databaseURL: 'https://auth-15fdf.firebaseio.com',
+            projectId: 'auth-15fdf',
+            storageBucket: '',
+            messagingSenderId: '509192538775',
+            appId: '1:509192538775:web:f7d4bb72c642067f',
+          });
+    }
+
     render() {
         return (
             <View>
+                <Header headerText="Authentication" />
                 <Text>
                     An App.
                 </Text>
